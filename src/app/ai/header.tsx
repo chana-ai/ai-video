e776692fe444835c430c9dd544521cb5de1d7598
Shijie Xu <shijiexu@yahoo.com>
2024-10-10 14:03:43 +0800

    change time

--- src/app/ai/header.tsx
+++ src/app/ai/header.tsx
@@ -14,7 +14,7 @@ export default function Header({
       <h1 className="text-xl font-semibold">{title}</h1>
       {children}
       <div className="ml-auto flex gap-1.5 text-sm text-red-500">
-        <h1>Public Alpha第一个版本运行时间: 10月7日-10月12号, 每天12:00--20:00; 每个用户每天最多10个成功的视频；请提前做好准备 </h1>
+        <h1>Public Alpha第一个版本运行时间: 10月7日-10月14号, 每天12:00--20:00; 每个用户每天最多10个成功的视频；请提前做好准备 </h1>
       </div>
       <div className="ml-auto flex gap-1.5 text-sm">
         <HeaderAccount />
